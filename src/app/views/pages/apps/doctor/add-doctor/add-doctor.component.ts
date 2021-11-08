bdca6bcf46d45b267171a35d7c76bec50d9a30d7
DESKTOP-CLV5B06\ACER <lakshaniishara95@gmail.com>
2021-11-08 19:44:42 +0530

    add doctor ui

--- src/app/views/pages/apps/doctor/add-doctor/add-doctor.component.ts
+++ src/app/views/pages/apps/doctor/add-doctor/add-doctor.component.ts
@@ -12,4 +12,17 @@ export class AddDoctorComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  openFileBrowser(event: any) {
+    event.preventDefault();
+    let element: HTMLElement = document.querySelector("#fileUploadInputExample") as HTMLElement;
+    element.click()
+  }
+
+  handleFileInput(event: any) {
+    if (event.target.files.length) {
+      let element: HTMLElement = document.querySelector("#fileUploadInputExample + .input-group .file-upload-info") as HTMLElement;
+      let fileName = event.target.files[0].name;
+      element.setAttribute( 'value', fileName)
+    }
+  }
 }
